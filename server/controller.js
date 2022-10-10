af16f883b7c2fcbebcd864d043fe9f9841d07424
Quinten Krauth <quintenkrauth19@gmail.com>
2022-10-10 16:53:04 -0400

    exam chamge 1

--- server/controller.js
+++ server/controller.js
@@ -1,13 +1,24 @@
-const data = require("./database")
+// I brought in my fruitData and just changed the name to "data" so that hopefully my formulas will stay in tact. I think the require part isnt working cuz my database isnt actually a json file and for that reason require("/") wont work
+const data = [
+    {id: 1, name: "apple"},
+    {id: 2, name: "orange"},
+    {id: 3, name: "banana"},
+    {id: 4, name: "lime"},
+    {id: 5, name: "grape"},
+]
+
+// const data = require("./database") <-- commenting this out for now
 let currentId = 5
+
 
 module.exports = {
     getFruit: (req, res) => res.status(200).send(data),
 
     postFruit: (req, res) => {
+        let {name} = req.body // <-- maybe this was the step my post function was missing
         let newFruit = {
             id: currentId,
-            name: postName.value,
+            name,
         }
         data.push(newFruit)
         res.status(200).send(data)
@@ -15,16 +26,16 @@ module.exports = {
     },
 
     putFruit: (req, res) => {
-        let {id, name} = req.params;
+        let {id} = req.params;
         let fruitUpdate = data.find(fruitData) = fruitData.id === parseInt(id)
         fruitUpdate.name = req.body.name;
         res.status(200)({message: "updated"}, data)
     },
 
     deleteFruit: (req, res) => {
-        let {id, name} = req.params;
-        let fruitDelete = data.find(fruitData) = fruitData.id === parseInt(req.params)
-        data.splice(req.params.id -1);
-        res.status(200)({message: "deleted"}, data);
+        // let {id, name} = req.params;
+        let fruitDelete = data.findIndex(elem => elem.id === +req.params.id) // <-- is this more syntactically precise? ♥‿♥
+        data.splice(req.params.id, 1);
+        res.status(200).send(data);
     }
 }
